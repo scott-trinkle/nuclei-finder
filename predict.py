68cc6cbcd38249ffa25100bcd1bb86e8dff3faaf
Scott Trinkle <tscott.trinkle@gmail.com>
2021-08-19 09:59:36 -0500

    Handle grayscale input, update augment demos

--- predict.py
+++ predict.py
@@ -2,14 +2,21 @@ import tensorflow as tf
 
 import numpy as np
 from skimage.io import imread
+from skimage.color import gray2rgb
 from model import get_model
 from scipy.ndimage import binary_fill_holes
 import matplotlib.pyplot as plt
 
 
 def img_to_tiles(img, tgt_shape):
-    tgt_h, tgt_w = tgt_shape
+    '''Takes an RGB or RGBA ndarray image (img) and returns a list of 
+    overlappingtiles of shape=tgt_shape'''
+    tgt_h, tgt_w, tgt_ch = tgt_shape
     img_h, img_w = img.shape[:2]
+
+    if len(img.shape) == 2:  # control for grayscale inputs
+        img = gray2rgb(img)
+    img = img[..., :tgt_ch]  # remove alpha channel if present
 
     nh_tiles = int(np.ceil(img_h / tgt_h))
     nw_tiles = int(np.ceil(img_w / tgt_w))
@@ -37,6 +44,8 @@ def img_to_tiles(img, tgt_shape):
 
 
 def tiles_to_img(tiles, img_shape):
+    '''Takes a list of overlapping tiles and composes them into a single image
+    with shape img_shape'''
     img_h, img_w = img_shape[:2]
     tgt_h, tgt_w = tiles[0].shape
 
@@ -67,19 +76,18 @@ def tiles_to_img(tiles, img_shape):
     return img
 
 
-def predict(imgs, IDs=None):
-    '''Expects a list of images
-    '''
+def predict(imgs, IDs=None, **predict_args):
+    '''Predicts nuclei masks from a list of images'''
 
     if IDs is None:
         IDs = [i for i in range(len(imgs))]
 
-    # Break imgs into a list of 128x128 tiles
+    # Break imgs into a list of 128x128x3 tiles
     img_tile_list = []
     ID_tile_list = []
     for i in range(len(imgs)):
-        img = imgs[i][..., :3]  # remove alpha channel if it exists
-        img_tiles = img_to_tiles(img, (128, 128))
+        img = imgs[i]
+        img_tiles = img_to_tiles(img, (128, 128, 3))
         img_tile_list.extend(img_tiles)
 
         ID_tile_list.extend([IDs[i]]*len(img_tiles))  # keep track of IDs
@@ -89,7 +97,7 @@ def predict(imgs, IDs=None):
     # Predict nuclei masks
     model = get_model()
     X = tf.convert_to_tensor(np.array(img_tile_list))
-    preds = model.predict(X)
+    preds = model.predict(X, **predict_args)
     preds_bin = (preds > 0.5).astype(np.uint8)
 
     # Put tiles back together
